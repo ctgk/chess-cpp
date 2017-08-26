ea206712d910bff4305d2d00c5a368b39c0184fa
ctgk <r0735nj5058@icloud.com>
2017-08-26 18:17:52 +0900

    displays checker board

--- gui.cpp
+++ gui.cpp
@@ -1,8 +1,50 @@
-#include "window.h"
+#include "gui.h"
+
+Board::Board(wxFrame *parent)
+    : wxPanel(parent, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxBORDER_NONE)
+{
+    statusbar = parent->GetStatusBar();
+    Connect(wxEVT_PAINT, wxPaintEventHandler(Board::OnPaint));
+}
+
+void Board::OnPaint(wxPaintEvent& event)
+{
+    wxPaintDC dc(this);
+
+    for (int i = 0; i < BoardLength; i++){
+        for (int j = 0; j < BoardLength; j++){
+            DrawSquare(dc, i, j);
+        }
+    }
+}
+
+int Board::SquareLength()
+{
+    int width = GetClientSize().GetWidth();
+    int height = GetClientSize().GetHeight();
+    return std::min(width, height) / BoardLength;
+}
+
+void Board::DrawSquare(wxPaintDC& dc, int x, int y)
+{
+    static wxColor light = wxColor(255, 222, 173);
+    static wxColor dark = wxColor(205, 133, 63);
+    int boardtop = GetClientSize().GetHeight() - BoardLength * SquareLength();
+
+    dc.SetPen(*wxTRANSPARENT_PEN);
+    if (x % 2 == y % 2){
+        dc.SetBrush(wxBrush(light));
+    } else {
+        dc.SetBrush(wxBrush(dark));
+    }
+    dc.DrawRectangle(x * SquareLength(), y * SquareLength(), SquareLength(), SquareLength());
+}
 
 Window::Window(const wxString& title)
-    : wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(300, 300))
+    : wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(500, 500))
 {
     wxStatusBar *sb = CreateStatusBar();
     sb->SetStatusText(wxT("White's Turn"));
+    Board *board = new Board(this);
+    board->SetFocus();
 }
--- gui.h
+++ gui.h
@@ -1,5 +1,20 @@
 #include <wx/wx.h>
 
+class Board : public wxPanel
+{
+public:
+    Board(wxFrame *parent);
+
+protected:
+    void OnPaint(wxPaintEvent& event);
+
+private:
+    enum { BoardLength = 8 };
+    int SquareLength();
+    void DrawSquare(wxPaintDC &dc, int x, int y);
+    wxStatusBar *statusbar;
+};
+
 class Window : public wxFrame
 {
 public:
