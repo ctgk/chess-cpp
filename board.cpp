59e25ffea97eb1261cfce1065e5067a56efb030d
ctgk <r0735nj5058@icloud.com>
2017-09-16 19:42:47 +0900

    added methods to return possible moves at a position

--- board.cpp
+++ board.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <cassert>
 #include <iostream>
 #include <sstream>
@@ -40,7 +41,7 @@ void Board::FEN2Board()
         }
     }
 
-    activeColor = fenBlocks[1];
+    activeColor = fenBlocks[1][0];
     castlability = fenBlocks[2];
     enPassantTarget = fenBlocks[3];
     halfmoveClock = std::stoi(fenBlocks[4]);
@@ -72,7 +73,8 @@ void Board::board2FEN()
         }
     }
 
-    fen += " " + activeColor;
+    fen += " ";
+    fen += activeColor;
     fen += " " + castlability;
     fen += " " + enPassantTarget;
     fen += " " + std::to_string(halfmoveClock);
@@ -81,41 +83,66 @@ void Board::board2FEN()
 
 char& Board::pieceAt(std::string notation)
 {
+    std::vector<int> indices = getIndices(notation);
+    return pieceAt(indices[0], indices[1]);
+}
+
+std::vector<int> Board::getIndices(std::string notation)
+{
     assert(notation.size() == 2);
     assert('a' <= notation[0] && notation[0] <= 'h');
     assert('1' <= notation[1] && notation[1] <= '8');
-    return pieceAt(7 - (notation[1] - '1'), notation[0] - 'a');
+    std::vector<int> indices;
+    indices.push_back(7 - (notation[1] - '1'));
+    indices.push_back(notation[0] - 'a');
+    return indices;
 }
 
-std::string Board::getPieceColor(char piece)
+std::string Board::getNotation(int i, int j)
+{
+    assert(0 <= i && i < Length);
+    assert(0 <= j && j < Length);
+    char file = 'a';
+    char rank = '1';
+    std::string notation;
+    file += j;
+    rank += Length - 1 - i;
+    notation += file;
+    notation += rank;
+    return notation;
+}
+
+char Board::getPieceColor(char piece)
 {
     if('A' < piece && piece < 'Z'){
-        return "w";
+        return 'w';
     } else if('a' < piece && piece < 'z'){
-        return "b";
+        return 'b';
     } else {
-        return "-";
+        return '-';
     }
 }
 
-std::string Board::getPieceColorAt(std::string notation)
+char Board::getPieceColorAt(std::string notation)
 {
     return getPieceColor(pieceAt(notation));
 }
 
-std::string Board::getPieceColorAt(int i, int j)
+char Board::getPieceColorAt(int i, int j)
 {
     return getPieceColor(pieceAt(i, j));
 }
 
 bool Board::move(std::string origin, std::string destination)
 {
-    std::string movingColor = getPieceColorAt(origin);
+    char movingColor = getPieceColorAt(origin);
     if(movingColor == activeColor){
+        std::vector<std::string> possibleMoves = getPossibleMovesAt(origin);
+        if(std::find(possibleMoves.begin(), possibleMoves.end(), destination) == possibleMoves.end()) return false;
         pieceAt(destination) = pieceAt(origin);
         pieceAt(origin) = '-';
         changeActiveColor();
-        if(movingColor == "b"){
+        if(movingColor == 'b'){
             fullmoveNumber += 1;
         }
         board2FEN();
@@ -124,7 +151,7 @@ bool Board::move(std::string origin, std::string destination)
     return false;
 }
 
-std::string getDestination(std::string origin, int fileDirection, int rankDirection)
+std::string Board::getDestination(std::string origin, int fileDirection, int rankDirection)
 {
     char originFile = origin[0];
     char originRank = origin[1];
@@ -142,19 +169,302 @@ std::string getDestination(std::string origin, int fileDirection, int rankDirect
     return destination;
 }
 
+std::vector<std::string> Board::getAttackingSquaresAt(std::string notation)
+{
+    char piece = pieceAt(notation);
+    std::vector<std::string> possibleMoves;
+    piece = std::tolower(piece, std::locale());
+    switch(piece){
+        case 'p':
+            return getPawnAttackingSquares(notation);
+        case 'n':
+            return getKnightAttackingSquares(notation);
+        case 'b':
+            return getBishopAttackingSquares(notation);
+        case 'r':
+            return getRookAttackingSquares(notation);
+        case 'q':
+            return getQueenAttackingSquares(notation);
+        case 'k':
+            return getKingAttackingSquares(notation);
+        default:
+            return possibleMoves;
+    }
+}
+
+std::vector<std::string> Board::getPossibleMovesAt(std::string notation)
+{
+    char piece = pieceAt(notation);
+    std::vector<std::string> possibleMoves;
+    piece = std::tolower(piece, std::locale());
+    switch(piece){
+        case 'p':
+            return getPawnPossibleMoves(notation);
+        case 'n':
+            return getKnightPossibleMoves(notation);
+        case 'b':
+            return getBishopPossibleMoves(notation);
+        case 'r':
+            return getRookPossibleMoves(notation);
+        case 'q':
+            return getQueenPossibleMoves(notation);
+        case 'k':
+            return getKingPossibleMoves(notation);
+        default:
+            return possibleMoves;
+    }
+}
+
+std::vector<std::string> Board::getPawnAttackingSquares(std::string notation)
+{
+    std::vector<std::string> attackingSquares;
+    std::string candidate;
+    int direction;
+    if(getPieceColorAt(notation) == 'w'){
+        direction = 1;
+    } else {
+        direction = -1;
+    }
+
+    for(int i = 0; i < 2; i++){
+        candidate = getDestination(notation, 2 * i - 1, direction);
+        if(candidate == "-") continue;
+        if(getPieceColorAt(candidate) != getPieceColorAt(notation))
+            attackingSquares.push_back(candidate);
+    }
+
+    return attackingSquares;
+}
+
+std::vector<std::string> Board::getPawnPossibleMoves(std::string notation)
+{
+    std::vector<std::string> possibleMoves;
+    std::string candidate;
+    int direction;
+    char opponentColor, homeRank;
+    if(getPieceColorAt(notation) == 'w'){
+        direction = 1;
+        opponentColor = 'b';
+        homeRank = '2';
+    } else {
+        direction = -1;
+        opponentColor = 'w';
+        homeRank = '7';
+    }
+
+    candidate = getDestination(notation, 0, direction);
+    if(candidate != "-"){
+        if(pieceAt(candidate) == '-'){
+            possibleMoves.push_back(candidate);
+            if(notation[1] == homeRank){
+                candidate = getDestination(notation, 0, 2 * direction);
+                if(pieceAt(candidate) == '-')
+                    possibleMoves.push_back(candidate);
+            }
+        }
+    }
+
+    for(int i = 0; i < 2; i++){
+        candidate = getDestination(notation, 2 * i - 1, direction);
+        if(candidate == "-") continue;
+        if(getPieceColorAt(candidate) == opponentColor)
+            possibleMoves.push_back(candidate);
+    }
+
+    return possibleMoves;
+}
+
+std::vector<std::string> Board::getKnightAttackingSquares(std::string notation)
+{
+    std::vector<std::string> possibleMoves;
+    std::string candidate;
+    static const int direction[8][2] = {
+        {-1, 2}, {1, 2},  // front (from white's perspective)
+        {2, 1}, {2, -1},  // right
+        {-1, -2}, {1, -2},  // back
+        {-2, 1}, {-2, -1}  // left
+    };
+
+    for(int i = 0; i < 8; i++){
+        candidate = getDestination(notation, direction[i][0], direction[i][1]);
+        if(candidate == "-") continue;
+        if(getPieceColorAt(candidate) != getPieceColorAt(notation))
+            possibleMoves.push_back(candidate);
+    }
+
+    return possibleMoves;
+}
+
+std::vector<std::string> Board::getKnightPossibleMoves(std::string notation)
+{
+    return getKnightAttackingSquares(notation);
+}
+
+std::vector<std::string> Board::getBishopAttackingSquares(std::string notation)
+{
+    std::vector<std::string> attackingSquares;
+    std::string candidate;
+    static const int direction[4][2] = {
+        {-1, 1}, {1, 1},
+        {-1, -1}, {1, -1}
+    };
+    char pieceColor = getPieceColorAt(notation);
+    char opponentColor, targetColor;
+    if(pieceColor == 'w'){
+        opponentColor = 'b';
+    } else {
+        opponentColor = 'w';
+    }
+
+    for(int i = 0; i < 4; i++){
+        candidate = notation;
+        while(true){
+            candidate = getDestination(candidate, direction[i][0], direction[i][1]);
+            if(candidate == "-") break;
+            targetColor = getPieceColorAt(candidate);
+            if(targetColor == pieceColor){
+                break;
+            } else if(targetColor == opponentColor){
+                attackingSquares.push_back(candidate);
+                break;
+            } else {
+                attackingSquares.push_back(candidate);
+            }
+        }
+    }
+
+    return attackingSquares;
+}
+
+std::vector<std::string> Board::getBishopPossibleMoves(std::string notation)
+{
+    return getBishopAttackingSquares(notation);
+}
+
+std::vector<std::string> Board::getRookAttackingSquares(std::string notation)
+{
+    std::vector<std::string> attackingSquares;
+    std::string candidate;
+    static const int direction[4][2] = {
+            {0, 1},
+        {-1, 0}, {1, 0},
+            {0, -1}
+    };
+    char pieceColor = getPieceColorAt(notation);
+    char opponentColor, targetColor;
+    if(pieceColor =='w'){
+        opponentColor = 'b';
+    } else {
+        opponentColor = 'w';
+    }
+
+    for(int i = 0; i < 4; i++){
+        candidate = notation;
+        while(true){
+            candidate = getDestination(candidate, direction[i][0], direction[i][1]);
+            if(candidate == "-") break;
+            targetColor = getPieceColorAt(candidate);
+            if(targetColor == pieceColor){
+                break;
+            } else if(targetColor == opponentColor){
+                attackingSquares.push_back(candidate);
+                break;
+            } else {
+                attackingSquares.push_back(candidate);
+            }
+        }
+    }
+
+    return attackingSquares;
+}
+
+std::vector<std::string> Board::getRookPossibleMoves(std::string notation)
+{
+    return getRookAttackingSquares(notation);
+}
+
+std::vector<std::string> Board::getQueenAttackingSquares(std::string notation)
+{
+    std::vector<std::string> attackingSquares = getBishopAttackingSquares(notation);
+    std::vector<std::string> tmp = getRookAttackingSquares(notation);
+    attackingSquares.insert(attackingSquares.end(), tmp.begin(), tmp.end());
+    return attackingSquares;
+}
+
+std::vector<std::string> Board::getQueenPossibleMoves(std::string notation)
+{
+    return getQueenAttackingSquares(notation);
+}
+
+std::vector<std::string> Board::getKingAttackingSquares(std::string notation)
+{
+    std::vector<std::string> attackingSquares;
+    std::string candidate;
+    static const int direction[8][2] = {
+        {-1, 1}, {0, 1}, {1, 1},
+        {-1, 0},         {1, 0},
+        {-1, -1}, {0, -1}, {1, -1}
+    };
+    char pieceColor = getPieceColorAt(notation);
+    char opponentColor, targetColor;
+    if(pieceColor =='w'){
+        opponentColor = 'b';
+    } else {
+        opponentColor = 'w';
+    }
+
+    for(int i = 0; i < 8; i++){
+        candidate = getDestination(notation, direction[i][0], direction[i][1]);
+        if(candidate == "-") break;
+        targetColor = getPieceColorAt(candidate);
+        if(targetColor == pieceColor){
+            continue;
+        } else {
+            attackingSquares.push_back(candidate);
+        }
+    }
+
+    return attackingSquares;
+}
+
+std::vector<std::string> Board::getKingPossibleMoves(std::string notation)
+{
+    return getKingAttackingSquares(notation);
+}
+
+void Board::print()
+{
+    char file = 'a';
+    for(int i = 0; i < Length; i++){
+        std::cout << 8 - i << ' ';
+        for(int j = 0; j < Length; j++){
+            std::cout << pieceAt(i, j) << ' ';
+        }
+        std::cout << std::endl;
+    }
+    std::cout << "  ";
+    for(int i = 0; i < Length; i++){
+        std::cout << file << ' ';
+        file += 1;
+    }
+    std::cout << std::endl << getFEN() << std::endl;
+}
+
 void Board::changeActiveColor()
 {
-    if(activeColor == "w"){
-        activeColor = "b";
+    if(activeColor == 'w'){
+        activeColor = 'b';
     } else {
-        activeColor = "w";
+        activeColor = 'w';
     }
 }
 
 // int main()
 // {
 //     Board board = Board();
-//     std::cout << board.getFEN() << std::endl;
+//     board.print();
 //     board.move("e2", "e4");
-//     std::cout << board.getFEN() << std::endl;
+//     board.print();
+//     board.move("e7", "e6");
+//     board.print();
 // }
--- board.h
+++ board.h
@@ -2,6 +2,7 @@
 #define BOARD_H
 
 #include <string>
+#include <vector>
 
 class Board
 {
@@ -11,23 +12,39 @@ public:
     void board2FEN();
     char& pieceAt(std::string notation);
     char& pieceAt(int i, int j) { return piecePlacement[i][j]; }
-    std::string getPieceColor(char piece);
-    std::string getPieceColorAt(std::string notation);
-    std::string getPieceColorAt(int i, int j);
+    std::vector<int> getIndices(std::string notation);
+    std::string getNotation(int i, int j);
+    char getPieceColor(char piece);
+    char getPieceColorAt(std::string notation);
+    char getPieceColorAt(int i, int j);
     bool move(std::string origin, std::string destination);
     std::string getDestination(std::string origin, int fileDirection, int rankDirection);
+    std::vector<std::string> getAttackingSquaresAt(std::string notation);
+    std::vector<std::string> getPossibleMovesAt(std::string notation);
+    std::vector<std::string> getPawnAttackingSquares(std::string notation);
+    std::vector<std::string> getPawnPossibleMoves(std::string notation);
+    std::vector<std::string> getKnightAttackingSquares(std::string notation);
+    std::vector<std::string> getKnightPossibleMoves(std::string notation);
+    std::vector<std::string> getBishopAttackingSquares(std::string notation);
+    std::vector<std::string> getBishopPossibleMoves(std::string notation);
+    std::vector<std::string> getRookAttackingSquares(std::string notation);
+    std::vector<std::string> getRookPossibleMoves(std::string notation);
+    std::vector<std::string> getQueenAttackingSquares(std::string notation);
+    std::vector<std::string> getQueenPossibleMoves(std::string notation);
+    std::vector<std::string> getKingAttackingSquares(std::string notation);
+    std::vector<std::string> getKingPossibleMoves(std::string notation);
     std::string getFEN() { return fen; }
-    std::string getActiveColor() { return activeColor; }
+    char getActiveColor() { return activeColor; }
     std::string getCastlability() { return castlability; }
     std::string getEnPassantTarget() { return enPassantTarget; }
     int getHalfmoveClock() { return halfmoveClock; }
     int getFullmoveNumber() { return fullmoveNumber; }
+    void print();
     static const int Length = 8;
-
 
 private:
     std::string fen;
-    std::string activeColor;
+    char activeColor;
     std::string castlability;
     std::string enPassantTarget;
     int halfmoveClock;
