01cf518dff2867c7cd7091b89880b24ced5fa0af
ctgk <r0735nj5058@icloud.com>
2017-09-17 18:43:09 +0900

    removed status text White's turn

--- frame.cpp
+++ frame.cpp
@@ -6,7 +6,6 @@ Frame::Frame(const wxString& title)
     : wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(480, 520))
 {
     wxStatusBar *sb = CreateStatusBar();
-    sb->SetStatusText(wxT("White's Turn"));
     Board *board = new Board();
     Panel *panel = new Panel(this, board);
     panel->SetFocus();
